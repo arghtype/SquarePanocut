e97b7c631f2a38eb9f19a73cd6daf4b457a22b0f
arghtype <arghtype@gmail.com>
2019-01-12 18:11:32 -0800

    Add Portrait (4x5) mode

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-SquarePanocut allows to cut panorama into squares. This is helpful for creating Instagram swipeable posts.
+SquarePanocut allows to cut panorama into squares (1x1) or portraits (4x5). This is helpful for creating Instagram swipeable posts.
 
-Requirements:
+####Requirements
+  `brew install python3`
+  
+  `pip3 install pillow`
 
-  brew install python3
-  pip3 install pillow
-
-Usage:
-  python3 squarepanocut.py panorama.jpg
-  python3 squarepanocut.py panorama.jpg left|right|center
-
-Last parameter defines which part of original panorama will be covered by resulting parts. Default value is 'left'. Total loss will be less than 1 image height.")
+####Usage
+  `python3 squarepanocut.py panorama.jpg` - default run, just slice pano into squares.
+  
+  `python3 squarepanocut.py panorama.jpg MODE` : MODE defines which part of original panorama will be covered by resulting parts. Possible values are `left`,`right`,`center`,  default  is `left`. Total loss will be less than 1 image height.
+  
+  `python3 squarepanocut.py panorama.jpg MODE p` : `p` flag is used to create portrait mode panorama - all resulting parts will be in 4x5 portrait format.
+  
--- squarepanocut.py
+++ squarepanocut.py
@@ -1,42 +1,50 @@
 import sys
 from PIL import Image
 
-if (len(sys.argv) != 2 and len(sys.argv) != 3):
-    print("SquarePanocut allows to cut panorama into squares. This is helpful for creating Instagram swipeable posts. \n\nUsage: \n python3 squarepanocut.py panorama.jpg\n python3 squarepanocut.py panorama.jpg left|right|center \nLast parameter defines which part of original panorama will be covered by resulting parts. Default value is 'left'. Total loss will be less than 1 image height.")
+args_length = len(sys.argv)
+if 2 > args_length > 4:
+    print("SquarePanocut allows to cut panorama into 1x1 squares or 4x5 portraits. "
+          "\n\nUsage: "
+          "\n python3 squarepanocut.py panorama.jpg"
+          "\n python3 squarepanocut.py panorama.jpg left|right|center"
+          "\n python3 squarepanocut.py panorama.jpg left|right|center p")
     exit()
 
 filename = sys.argv[1]
-print("Working with " + filename)
-
-image  = Image.open(filename)
+image = Image.open(filename)
 width, height = image.size
-
+step = height
 offset = 0
 
-if (len(sys.argv) == 3):
+if args_length == 4:
+    step = step * 4 // 5
+if args_length >= 3:
     mode = sys.argv[2]
-    if ("left" == mode):
+    if "left" == mode:
         offset = 0
     else:
         reminder = width
-        while reminder >= height:
-            reminder -= height
-        if ("right" == mode):
+        while reminder >= step:
+            reminder -= step
+        if "right" == mode:
             offset = reminder
-        if ("center" == mode):
+        if "center" == mode:
             offset = reminder // 2
-
-count = width // height
+count = width // step
 i = 0
 
+print("Working with " + filename + " of size " + str(width) + "x" + str(height))
+print("Slicing into " + str(count) + " parts of size " + str(step) + "x" + str(height))
+
 while i < count:
-    left = offset + i * height
+    left = offset + i * step
     top = 0
-    right = offset + height + i * height
+    right = offset + step + i * step
     low = height
-    print("Cropping Part " + str(i) + ":   " +str(left) + ", " +str(top) + ", " +str(right) + ", " + str(low))
+    output_name = "part" + str(i) + ".jpg"
+    print("Part " + str(i) + ":\t" +str(left) + ",\t" +str(top) + ",\t" +str(right) + ",\t" + str(low) + "\t> " + output_name)
     area = (left, top, right, low)
     part = image.crop(area)
-    part.save("part" + str(i) + ".jpg", quality = 100)
+    part.save(output_name, quality=100)
     i += 1
 print("Success!")
